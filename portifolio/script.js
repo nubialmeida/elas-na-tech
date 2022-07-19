8e783fb82054dfaa3a2f46c2d7c81c6d9dd37793
Nubia Almeida <nubyalmeida45@gmail.com>
2022-07-19 12:52:50 -0300

    adicionando estilos

--- portifolio/script.js
+++ portifolio/script.js
@@ -19,3 +19,34 @@ function selectSection(option) {
         el.dataset.section === option.dataset.select ? showElement(el) : hideElement(el)
     );
 }
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+    showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "flex";
+    dots[slideIndex - 1].className += " active";
+}
